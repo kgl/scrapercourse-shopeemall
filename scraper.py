a4f4ec288e1ecff48a88d8ce5efda6863ccb10b9
kgl <john.tw@gmail.com>
2022-06-05 20:12:51 +0800

    5.3批次爬取動態網頁的資料

--- scraper.py
+++ scraper.py
@@ -2,12 +2,15 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.common.action_chains import ActionChains
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
 driver.get(
     'https://shopee.tw/mall/%E6%9B%B8%E7%B1%8D%E5%8F%8A%E9%9B%9C%E8%AA%8C%E6%9C%9F%E5%88%8A-cat.11041120')
 time.sleep(5)
+
+ActionChains(driver).move_by_offset(100, 100).click().perform()
 
 cards = driver.find_elements(
     By.CSS_SELECTOR, "div[class='col-xs-2 recommend-products-by-view__item-card-wrapper']")
