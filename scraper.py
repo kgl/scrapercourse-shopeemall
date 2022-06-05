36231a87f2dc4aa9d702e5277f256b29efe3eda0
kgl <john.tw@gmail.com>
2022-06-05 21:37:58 +0800

    5.8 3個常見的動態網頁等待機制

--- scraper.py
+++ scraper.py
@@ -3,6 +3,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
@@ -10,9 +12,13 @@ for page in range(1, 3):
 
     driver.get(
         f'https://shopee.tw/mall/%E6%9B%B8%E7%B1%8D%E5%8F%8A%E9%9B%9C%E8%AA%8C%E6%9C%9F%E5%88%8A-cat.11041120/popular?pageNumber={page}')
-    time.sleep(5)
 
     ActionChains(driver).move_by_offset(100, 100).click().perform()
+
+    locator = (By.CSS_SELECTOR,
+               "div[class='col-xs-2 recommend-products-by-view__item-card-wrapper']")
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located(locator), "找不到指定的元素")
 
     cards = driver.find_elements(
         By.CSS_SELECTOR, "div[class='col-xs-2 recommend-products-by-view__item-card-wrapper']")
